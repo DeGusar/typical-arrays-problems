501637f87700f1ed119a8c60b4fac047ae5ee75d
Denis Gusar <degusar.wot@gmail.com>
2021-08-22 15:56:09 +0300

    feat: finish typical-arrays-problems task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    
+ return (typeof array !== 'undefined' && array.length !==0) ? Math.min(...array) : 0;
 }
 
 exports.max = function max (array) {
-  return 0;
+  return (typeof array !== 'undefined' && array.length !==0) ? Math.max(...array) : 0;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (typeof array !== 'undefined' && array.length !== 0) {
+        let result = array.reduce(function (sum, elem) {
+            return sum + elem;
+        }, 0);
+        return result / array.length;
+    } else return 0;
 }
